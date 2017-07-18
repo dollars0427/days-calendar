11ed545865a7421ad530d22438fdf6d753afabf6
Sardo <sardo@pixelactionstudio.com>
2017-07-18 17:17:54 +0800

    Init commit

---
+++ src/components/calendar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="calendar">
+    <div class="calendar-header">
+      <div class="month">
+        July 2017
+      </div>
+    </div>
+    <div class="calendar-body">
+
+    </div>
+  </div>
+</template>
+
+ <script>
+   export default {
+
+   }
+   </script>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import calendar from './components/calendar.vue';
+
+new Vue({
+  el: '#app',
+   components: {
+      calendar
+   }
+})
