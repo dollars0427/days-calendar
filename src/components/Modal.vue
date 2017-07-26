f7f011237fba2818e5d2c75dc0fd2a5a4e3da8d1
Sardo <sardo@pixelactionstudio.com>
2017-07-26 11:45:10 +0800

    Add default content of modal and body

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -2,33 +2,44 @@
   <div id="calendar-modal">
     <span :class="day.class" v-on:click="show">{{day.number}}</span>
     <transition name="modal">
-    <div v-if="showModal">
+      <div v-if="showModal">
         <div class="modal-mask" v-on:click="close">
           <div class="modal-wrapper">
             <div class="modal-container">
               <div class="modal-header">
                 <slot name="header">
-                  Date
+                  {{ getDate }}
                 </slot>
               </div>
 
               <div class="modal-body">
                 <slot name="body">
-                  Date Content
+                  No events.
                 </slot>
               </div>
             </div>
           </div>
         </div>
-    </div>
+      </div>
     </transition>
   </div>
 </template>
 
 <script>
+import moment from 'moment';
+
 export default {
   props: {
     'day': Object,
+  },
+  computed: {
+    getDate: function () {
+      if(this.day.date.format){
+        return this.day.date.format("MMM Do YY");
+      }else{
+        return this.day.date;
+      }
+    }
   },
   methods: {
     show: function () {
